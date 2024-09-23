399704dfbe2e17e18db7280beb5ed4e106dd6889
Justin Green <justingreendev@gmail.com>
2024-09-22 19:57:36 -0500

    closure example

---
+++ closure.js
@@ -0,0 +1,18 @@
+const createCounter = function(n) {
+    let count = n;
+    return function() {
+        console.log(count++);
+    };
+};
+
+const counter = createCounter(10);
+ counter() // 10
+ counter() // 11
+ counter() // 12
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
