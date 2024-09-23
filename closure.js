02b3e92d86596c5103c682658062e347e53ccafb
Justin Green <justingreendev@gmail.com>
2024-09-22 19:58:01 -0500

    remove comments

--- closure.js
+++ closure.js
@@ -9,10 +9,3 @@ const counter = createCounter(10);
  counter() // 10
  counter() // 11
  counter() // 12
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
