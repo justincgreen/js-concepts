5a530a2586757e82c9bc8ccbfd1af5a807757a07
Justin Green <justingreendev@gmail.com>
2024-09-24 10:05:21 -0500

    variable scopes

--- blockScope.js
+++ blockScope.js
@@ -1,5 +1,6 @@
 /*
   Block Scope
+  This scope restricts the variable that is declared inside a specific block, from access by the outside of the block. The let & const keyword facilitates the variables to be block scoped. In order to access the variables of that specific block, we need to create an object for it. Variables declared with the var keyword, do not have block scope.
 */
 function blockScopeExample() {
   let outsideBlock = 'I am outside the block';
