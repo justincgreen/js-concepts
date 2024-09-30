ea41d9b7028eda9ed4b5269eb8fa84e077f0083a
Justin Green <justingreendev@gmail.com>
2024-09-30 11:50:52 -0500

    string operators

---
+++ stringOperators.js
@@ -0,0 +1,5 @@
+//console.log("my " + "string"); // console logs the string "my string".
+let myString = 'alpha';
+myString += 'bet'; // alphabet and sets myString to alphabet
+
+console.log(myString);
