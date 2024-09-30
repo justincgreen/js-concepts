49f007113f6007ad9d340e92033ab262de916d15
Justin Green <justingreendev@gmail.com>
2024-09-30 16:46:27 -0500

    this

--- thisMethod.js
+++ thisMethod.js
@@ -1,5 +1,6 @@
 /*
-  this keyword when used in a method relates to the object it's in
+  this keyword when used in a method relates to the object it's in.
+  you cannot change the value of this only where its referenced with call(), apply(), and bind()
 */
 const person = {
   firstName: 'Hank',
