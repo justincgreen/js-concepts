046d523b2fff8593089812d3ab7210f3832a9661
Justin Green <justingreendev@gmail.com>
2024-09-30 21:57:06 -0500

    Promise example

--- promises.js
+++ promises.js
@@ -2,3 +2,13 @@
   Promise
   A promise is an object that represents the success or failure of a given operation, usually an asynchronous operation.
 */
+
+const timerPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hello');
+  }, 2000)
+});
+
+timerPromise.then((response) => {
+  console.log(response);
+})
