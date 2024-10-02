2ce4d60162f05722b33cd037044d9b3699f79258
Justin Green <justingreendev@gmail.com>
2024-10-02 10:37:47 -0500

    notes

--- iife.js
+++ iife.js
@@ -1,5 +1,6 @@
 /*
   IIFE (Immediately Invoked Function Expression)
+  This can be useful for setting up variables, initializing settings, or executing logic without leaving any trace of the function in the global scope.
 */
 
 (
